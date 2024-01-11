d23107c81afebd4a9c7f921b106b893ceb44b138
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 21:07:12 +0200

    SQL FILES

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -10,9 +10,10 @@ CREATE TABLE IF NOT EXISTS first_table (
 );
 
 -- Create records
-INSERT INTO first_table (id, name) VALUES (1, "A");
-INSERT INTO first_table (id, name) VALUES (89, "B");
-INSERT INTO first_table (id, name) VALUES (2, "C");
-INSERT INTO first_table (id, name) VALUES (89, "D");
-INSERT INTO first_table (id, name) VALUES (89, "E");
-INSERT INTO first_table (id, name) VALUES (3, "F");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (3, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
