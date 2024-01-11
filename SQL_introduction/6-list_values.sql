4345af11840489bd2a89e77942a70b337a895aa6
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 20:32:42 +0200

    SQL FILE

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,1 +1,16 @@
---A script that lists all row of the table
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_6;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_6;
+USE hbtn_test_db_6
+
+-- Create table
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Insert 3 records
+INSERT INTO first_table (id, name) VALUES (1, "First name");
+INSERT INTO first_table (id, name) VALUES (2, "Last name");
+INSERT INTO first_table (id, name) VALUES (3, "name")
