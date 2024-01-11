7979d86de07a14841ca499bded3a642f79e9edcc
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 21:54:56 +0200

    sql files

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,5 +3,3 @@ DROP DATABASE IF EXISTS hbtn_test_db_8;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
 USE hbtn_test_db_8;
 
--- Display second_table
-SELECT id, name, score FROM second_table ORDER BY id ASC
