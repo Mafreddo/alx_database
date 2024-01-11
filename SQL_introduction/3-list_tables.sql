6b632414214a5903d18c7e9afb91e2678e936298
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 20:18:04 +0200

    SQL FILES

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -4,14 +4,17 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (
+    name VARCHAR(256)
     id INT
 );
 
 CREATE TABLE IF NOT EXISTS holbteron_1 (
     name VARCHAR(256)
+    id INT
 );
 
 CREATE TABLE IF NOT EXISTS holbteron_2 (
     id INT,
     name VARCHAR(256)
+    age INT
 );
