61c035ab7f0fe90923131f967f8f2a0719623482
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 21:57:26 +0200

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,3 +3,20 @@ DROP DATABASE IF EXISTS hbtn_test_db_8;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
 USE hbtn_test_db_8;
 
+-- Create database
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Create records
+INSERT INTO second_table (id, name, score) VALUES (11, "A", 12);
+INSERT INTO second_table (id, name, score) VALUES (12, "B", 12);
+INSERT INTO second_table (id, name, score) VALUES (13, "C", 12);
+INSERT INTO second_table (id, name, score) VALUES (14, "D", 12);
