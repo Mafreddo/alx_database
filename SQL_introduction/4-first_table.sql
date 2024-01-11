9847519bc2ee6697e25c931a1b62ef15ff41a231
Mafreddo <sello.mathekga334@gmail.com>
2024-01-11 19:37:04 +0200

    Alx files

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,1 +1,10 @@
 --a script that creates a table called of a first table
+
+CREATE DATABASE IF NOT EXISTS MySQL;
+
+USE MySQL
+CREATE TABLE first_table (
+    name varchar(256)
+    id INT
+    age 27
+)
