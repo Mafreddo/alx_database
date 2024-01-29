c4dff1d4086a607399bb2da062f8402b6c88835c
Mafreddo <sello.mathekga334@gmail.com>
2024-01-29 19:56:08 +0200

    HOPE

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
 -- Creating a user on Databased
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILAGES;
+
+CREATE USER 'user_0d_2'@'localhost' iDENTIFIED BY 'password';
+GRANT ALL ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILAGES;
