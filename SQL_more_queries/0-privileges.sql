f2a1523143ff5b7b50d9f9afa314c3af47ae62dd
Mafreddo <sello.mathekga334@gmail.com>
2024-01-29 17:57:00 +0200

    Lets see

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
 -- Delete user user_0d_1 and user_0d_2 if exist
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 DROP USER IF EXISTS 'user_0d_2'@'localhost'
-GRANT SELECT *.*
-ON localhost.*;
-TO user_0d_1 AND user_0d_2
