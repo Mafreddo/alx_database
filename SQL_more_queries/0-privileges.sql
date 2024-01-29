e2cce41a3d45f5b571b4bb9939b69c952165d286
Mafreddo <sello.mathekga334@gmail.com>
2024-01-29 17:50:23 +0200

    alx sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 --This is a script that grants previlages
-CREATE USER user_0d_1
-CREATE USER user_0d_2
+CREATE USER user_0d_1 @localhost iF DOES NOT EXIST
+CREATE USER user_0d_2 @localhost IF DOES NOT EXIST
 GRANT SELECT *.*
 ON localhost.*;
-TO user_0d_1,user_0d_2
+TO user_0d_1
