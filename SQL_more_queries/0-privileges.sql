36622b5a6264e30056b42065a300fe0f25d81a3a
Mafreddo <sello.mathekga334@gmail.com>
2024-01-29 17:59:02 +0200

    Lets Go

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Delete user user_0d_1 and user_0d_2 if exist
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost'
+-- Creating user user_0d_1 and user_0d_2 if exist
+CREATE USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF EXISTS 'user_0d_2'@'localhost'
