e8a7902a6ad0cf4fa3da998d8dc14ee9f028d657
Mafreddo <sello.mathekga334@gmail.com>
2024-02-02 10:49:27 +0200

    ALX

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,4 +4,6 @@ CREATE USER 'user_0d_1'@'localhost';
 
 SET PASSWORD FOR 'user_0d_1'@'localhost'='user_0d_1_pwd';
 
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+
+FLUSH PRIVILAGES;
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 --Creating database and user
 
-CREATE USER 'user_0d_2'@'hbtn_0d_2' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXIST 'user_0d_2'@'hbtn_0d_2' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
 
 GRANT SELECT 
     -> ON *.*
