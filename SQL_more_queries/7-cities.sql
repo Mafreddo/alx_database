ef3a65e08d261b90b126b7a447e496c9c62c9f3f
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 15:42:25 +0200

    A

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- Creating Database and Tables
 
-USE  hbtn_0d_usa;
-CREATE TABLE citieS
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
     id INT UNIQUE AUTO GENERATED NOT NULL PRIMARY KEY 
-    state_id INT NOT NULL FOREIGN KEY
+    state_id INT
     name VARCHAR(256) NOT NULL
+    FOREIGN KEY USE (state_id) REFERENCES states (id) 
 )
