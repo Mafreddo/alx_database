6137487fd01bf32371e5e02ac890921466295af8
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 16:19:54 +0200

    alx

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creating database and user
 
-CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* TO 'user_0d_2'@'hbtn_0d_2';
