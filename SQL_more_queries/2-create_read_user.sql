e4be90f8359a7a83bf280c5541ac80ee2c02fc3b
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 19:45:00 +0200

    a

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Creating database and user
 
-DROP USER 'user_0d_2'@'localhost';
+--DROP USER 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost'
-GRANT SELECT 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+--GRANT USAGE ON *.* TO 'user_0d_2'@'localhost'
+--GRANT SELECT 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
