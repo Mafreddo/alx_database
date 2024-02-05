77bc125b3e19ce7a27cf2b69fb6d24459c57414d
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 10:08:15 +0200

    alx files

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- user_0d_1 root user and user_0d_2
+DROP USER IF EXISTS user_0d_1 @ localhost;
+DROP USER IF EXISTS user_0d_2 @ localhost;
 
 CREATE IF NOT EXISTS 'user_0d_1' @ 'localhost';
 CREATE IF NOT EXISTS 'user_0d_2' @ 'localhost';
