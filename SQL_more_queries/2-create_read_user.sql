74475b14a55708c45bf5747c4cae3c8e61cc13d0
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 04:20:10 +0200

    user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 --Creating database and user
 
-CREATE USER IF NOT EXIST 'user_0d_2'@'hbtn_0d_2' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXIST 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd'
 
 GRANT SELECT 
     -> ON *.*
