cdaa2104b4837b32c12fdd05ae8c85afaaee4608
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 11:30:37 +0200

    v

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,10 +4,9 @@ USE hbtn_0d_usa;
 
 CREATE TABLE states(
     id INT,
-    cities_id INT,
     name VARCHAR(15),
 );
 INSERT INTO states
-VALUES (1,1, "San Fransisco"), (2,1, "San Jose"), (4,2,"Page");
+VALUES (1, "San Fransisco"), (2, "San Jose"), (4,"Page");
 
 SELECT * FROM states;
