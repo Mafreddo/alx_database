fe83452513b7e4d90331a341fc82ef95804f46ed
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 15:52:58 +0200

    as

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
 -- user_0d_1 root user and user_0d_2
-DROP USER IF EXISTS user_0d_1 @ localhost;
-DROP USER IF EXISTS user_0d_2 @ localhost;
 
-CREATE IF NOT EXISTS 'user_0d_1' @ 'localhost';
-CREATE IF NOT EXISTS 'user_0d_2' @ 'localhost';
-
-SHOW ALL PRIVILAGES @'user_0d_1' AND 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'suer_0d_2'@'localhost';
