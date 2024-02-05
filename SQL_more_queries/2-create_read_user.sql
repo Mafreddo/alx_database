390e3b4db745fa64ab069ae0d47b832962330d82
Mafreddo <sello.mathekga334@gmail.com>
2024-02-05 04:21:46 +0200

    alx

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 --Creating database and user
 
 CREATE USER IF NOT EXIST 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd'
+SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd';
 
 GRANT SELECT 
-    -> ON *.*
+    -> ON *.*;
     -> TO user_0d_2@hbtn_0d_2;
