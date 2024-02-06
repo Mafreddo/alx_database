85a293fc17c49593002c9b6ee756fa94ecb9778f
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 15:48:05 +0200

    ALC

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,7 +2,12 @@
 USE hbtn_0d_2;
 CREATE TABLE id_not_null
 (
-
     id INT with the default value 1
     name VARCHAR(256)
 )
+INSERT TO id_not_null (id, name)
+VALUE(1, Califonia)
+INSERT TO id_not_null (id, name)
+VALUE(2, San Franscisco)
+INSERT TO id_not_null (id, name)
+VALUE(3, Page)
