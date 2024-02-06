b5a9e0da5b021b8ae69ae72ae55e32ed34e9ffcf
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 09:34:52 +0200

    ALX

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Creating database and user
 
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT *.* TO 'user_0d_2'@'localhost';
