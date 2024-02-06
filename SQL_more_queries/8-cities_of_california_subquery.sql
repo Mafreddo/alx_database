8e2796b70193baa460497906b50a196a9d9122e9
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 10:40:11 +0200

    alx

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,3 @@
 -- Creating Database of all the cities in Califonia
 
-CREATE TABLE states(
-    id INT,
-    name VARCHAR(15),
-);
-INSERT INTO states
-VALUES (1, "San Fransisco"), (2, "San Jose"), (4,"Page");
-
 SELECT * FROM states;
