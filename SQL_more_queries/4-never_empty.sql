4d6414cedb486e0ef4a45af115d4f08e4676ecf1
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 16:17:17 +0200

    D

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,16 +1,12 @@
 --Creating anonther table
-CREATE TABLE IF NOT EXISTS `id_not_null`;
+CREATE TABLE IF NOT EXISTS `id_not_null`
 (
-    id INT with the default value 1
-    name VARCHAR(256)
+    id (INT with the default value 1),
+    name (VARCHAR(256))
 );
 INSERT INTO `id_not_null` (id, name);
-VALUE(1, `Helberton School`);
-INSERT TO `id_not_null` (id, name);
-VALUE(1, `Python is cool`);
-INSERT TO `id_not_null` (id, name);
-VALUE(2, `Holberton`);
-INSERT TO `id_not_null` (id, name);
-VALUE(3, `School`);
-INSERT TO `id_not_null` (id, name);
-VALUE(4, `C is fun`);
+VALUE(1, `Helberton School`),
+(1, `Python is cool`),
+(2, `Holberton`),
+(3, `School`),
+(4, `C is fun`);
