6cf5548b93cc3b84311a80332f0e092827546e1d
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 16:05:09 +0200

    C

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `id_not_null`;
 (
     id INT with the default value 1
     name VARCHAR(256)
-)
+);
 INSERT INTO `id_not_null` (id, name);
 VALUE(1, `Helberton School`);
 INSERT TO `id_not_null` (id, name);
