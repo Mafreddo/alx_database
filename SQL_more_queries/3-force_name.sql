e67107ac7c15afcaea1f08addc316b9d649ec7f2
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 14:10:53 +0200

    als

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,8 +1,5 @@
 --Creating a table force name
-
-CREATE TABLE IF NOT EXISTS foece name
-(
-    id INT 
-    name VARCHAR(256) NOT NULL 
-
+CREATE TABLE IF NOT EXISTS force_name (
+  id INT,
+  name VARCHAR(256) NOT NULL
 )
