eb990b891cfa831900678bb5db4bae3d36a542b3
Mafreddo <sello.mathekga334@gmail.com>
2024-02-06 15:56:54 +0200

    aLX

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,13 +1,17 @@
 --Creating anonther table
 USE hbtn_0d_2;
-CREATE TABLE id_not_null
+CREATE TABLE IF NOT EXISTS id_not_null;
 (
     id INT with the default value 1
     name VARCHAR(256)
 )
-INSERT TO id_not_null (id, name)
-VALUE(1, Califonia)
-INSERT TO id_not_null (id, name)
-VALUE(2, San Franscisco)
-INSERT TO id_not_null (id, name)
-VALUE(3, Page)
+INSERT INTO id_not_null (id, name);
+VALUE(1, Helberton School);
+INSERT TO id_not_null (id, name);
+VALUE(1, Python is cool);
+INSERT TO id_not_null (id, name);
+VALUE(2, Holberton);
+INSERT TO id_not_null (id, name);
+VALUE(3, School);
+INSERT TO id_not_null (id, name);
+VALUE(4, C is fun);
